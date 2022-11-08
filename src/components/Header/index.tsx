0d0446cc69ddaffbc232551605a65c8606257fe6
solebellsBEACH <lucassxxavier@gmail.com>
2022-11-08 09:42:41 -0300

    🔥improving headerLayout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Container, ContentLeft, ContentRight, Links, Logo, Title } from './styles'
 import EtheriumLogo from '../../assets/etherium_logo.svg'
+import { Grid, SimpleGrid, WrapItem } from '@chakra-ui/react'
 
 export const Header = () => {
-    const links: { name: string, url: string }[] = [{ name: 'Container', url: 'home' }]
+    const links: { name: string, url: string }[] = [{ name: 'Container', url: 'home' }, { name: 'Container', url: 'home' }, { name: 'Container', url: 'home' }, { name: 'Container', url: 'home' }]
     return (
         <Container>
             <ContentLeft><Logo src={EtheriumLogo} /><Title>Stock<h1>Bro</h1>kerage</Title></ContentLeft>
             <ContentRight>
-                {links.map(e => <Links onClick={() => { console.log(e) }}>{e.name}</Links>)}
+                <SimpleGrid
+                    columns={[3, links.length]} spacing={2}
+                >{links.map((e, i) => <WrapItem key={'links' + i}><Links onClick={() => { console.log(e) }}>{e.name}</Links></WrapItem>)}
+                </SimpleGrid>
             </ContentRight>
         </Container>
     )
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -9,6 +9,11 @@ display:flex ;
 align-items: center;
 justify-content: space-between;
 padding: 0 3vw;
+
+@media(max-width:730px){
+    flex-direction: column;
+    align-items: flex-start;
+}
 `
 
 export const Title = styled.h1`
@@ -27,12 +32,16 @@ h1{
 export const ContentLeft = styled.div`
 display:flex ;
 align-items: center;
-margin-right:10rem;
+
+@media (max-width:290px){
+    flex-direction: column;
+    margin-right:3rem;
+}
 `
 export const ContentRight = styled.div`
-min-height:5rem;
-display:flex ;
-align-items: center;
+@media(max-width:730px){
+    margin-bottom:1rem;
+}
 `
 export const Logo = styled.img`
 height:4rem;
@@ -42,5 +51,8 @@ font-weight:900 ;
 font-size:15px;
 color:${props => props.theme.white} ;
 text-transform:uppercase;
-margin:0 1rem;
+margin:0 0.5rem;
+@media (max-width:290px){
+    font-size:10px;
+}
 `
